39837c2155de64e8c64b7700d56ce6fc2a8454a2
DeCode2018 <decode2018@gmail.com>
2019-01-01 20:21:43 -0500

    added basic form validation to disable submit buttons (new contact and new event) unless all inputs fields are filled out

--- reconnect-frontend/src/component/NewContactForm.js
+++ reconnect-frontend/src/component/NewContactForm.js
@@ -12,9 +12,61 @@ class NewContactForm extends Component{
      this.state = {
        selectedBirthDate: undefined,
        selectedEventDate: undefined,
-       user_id: this.props.userInfo.id
+       user_id: this.props.userInfo.id,
+
+
+       first_name:'',
+       last_name:'',
+       bday:'',
+       contact_avatar:'',
+       category:'',
+       last_event_date:'',
+       relationship:'',
+       home_address:'',
+       home_city:'',
+       home_state:'',
+       home_zip:'',
+       company_name:'',
+       job_title:'',
+       company_address:'',
+       company_city:'',
+       company_state:'',
+       company_zip:'',
+       cell_num:'',
+       email_address:'',
+       notes:'',
+
+       formError:false,
+       first_nameError:false,
+       last_nameError:false,
+       bdayError:false,
+       contact_avatarError:false,
+       categoryError:false,
+       last_event_dateError:false,
+       relationshipError:false,
+       home_addressError:false,
+       home_cityError:false,
+       home_stateError:false,
+       home_zipError:false,
+       company_nameError:false,
+       job_titleError:false,
+       company_addressError:false,
+       company_cityError:false,
+       company_stateError:false,
+       company_zipError:false,
+       cell_numError:false,
+       email_addressError:false,
+       notesError:false
      };
    }
+
+   onInputChange = (event) => {
+
+     this.setState({
+       [event.currentTarget.name]:event.currentTarget.value
+     })
+   }
+
    handleBirthDateChange(day) {
     let  bdate = new Date(day);
     let  byear = bdate.getFullYear();
@@ -50,6 +102,157 @@ class NewContactForm extends Component{
 
    handleSubmit =(event)=>{
       event.preventDefault();
+      let error = false;
+
+      if (this.state.first_name === ''){
+        this.setState({first_nameError:true});
+        error=true;
+      } else {
+        this.setState({first_nameError:false});
+      }
+
+      if (this.state.last_name === ''){
+        this.setState({last_nameError:true});
+        error=true;
+      } else {
+        this.setState({last_nameError:false});
+      }
+
+      if (this.state.selectedBirthDate === undefined){
+        this.setState({bdayError:true});
+        error=true;
+      } else {
+        this.setState({bdayError:false});
+      }
+
+      if (this.state.contact_avatar === ''){
+        this.setState({contact_avatarError:true});
+        error=true;
+      } else {
+        this.setState({contact_avatarError:false});
+      }
+
+      if (this.state.category === ''){
+        this.setState({categoryError:true});
+        error=true;
+      } else {
+        this.setState({categoryError:false});
+      }
+
+      if (this.state.selectedEventDate === undefined){
+        this.setState({last_event_dateError:true});
+        error=true;
+      } else {
+        this.setState({last_event_dateError:false});
+      }
+
+      if (this.state.relationship === ''){
+        this.setState({relationshipError:true});
+        error=true;
+      } else {
+        this.setState({relationshipError:false});
+      }
+
+      if (this.state.home_address === ''){
+        this.setState({home_addressError:true});
+        error=true;
+      } else {
+        this.setState({home_addressError:false});
+      }
+
+      if (this.state.home_city === ''){
+        this.setState({home_cityError:true});
+        error=true;
+      } else {
+        this.setState({home_cityError:false});
+      }
+
+      if (this.state.home_state === ''){
+        this.setState({home_stateError:true});
+        error=true;
+      } else {
+        this.setState({home_stateError:false});
+      }
+
+      if (this.state.home_zip === ''){
+        this.setState({home_zipError:true});
+        error=true;
+      } else {
+        this.setState({home_zipError:false});
+      }
+
+      if (this.state.company_name === ''){
+        this.setState({company_nameError:true});
+        error=true;
+      } else {
+        this.setState({company_nameError:false});
+      }
+
+      if (this.state.job_title === ''){
+        this.setState({job_titleError:true});
+        error=true;
+      } else {
+        this.setState({job_titleError:false});
+      }
+
+      if (this.state.company_address === ''){
+        this.setState({company_addressError:true});
+        error=true;
+      } else {
+        this.setState({company_addressError:false});
+      }
+
+      if (this.state.company_city === ''){
+        this.setState({company_cityError:true});
+        error=true;
+      } else {
+        this.setState({company_cityError:false});
+      }
+
+      if (this.state.company_state === ''){
+        this.setState({company_stateError:true});
+        error=true;
+      } else {
+        this.setState({company_stateError:false});
+      }
+
+      if (this.state.company_zip === ''){
+        this.setState({company_zipError:true});
+        error=true;
+      } else {
+        this.setState({company_zipError:false});
+      }
+
+      if (this.state.cell_num === ''){
+        this.setState({cell_numError:true});
+        error=true;
+      } else {
+        this.setState({cell_numError:false});
+      }
+
+      if (this.state.email_address === ''){
+        this.setState({email_addressError:true});
+        error=true;
+      } else {
+        this.setState({email_addressError:false});
+      }
+
+      if (this.state.notes === ''){
+        this.setState({notesError:true});
+        error=true;
+      } else {
+        this.setState({notesError:false});
+      }
+
+      if (error){
+        this.setState({formError:true});
+        return
+      }
+
+      this.setState({formError:false})
+
+
+
       const data = new FormData(event.target)
       fetch('http://localhost:3000/api/v1/contacts', {
       method: 'POST',
@@ -87,6 +290,8 @@ class NewContactForm extends Component{
   render(){
     const { selectedBirthDate } = this.state;
     const { selectedEventDate } = this.state;
+    const cutOffDate = new Date()
+    console.log(cutOffDate)
   return(
     <div className="ui main container grid four wide column centered">
     <form onSubmit={this.handleSubmit} className="ui form six wide column centered" >
@@ -95,10 +300,16 @@ class NewContactForm extends Component{
       <label>Name</label>
       <div className="two fields">
       <div className="field">
-        <input type="text" id="first_name" name="first_name" placeholder="First Name"/>
+        <input type="text" id="first_name" name="first_name" placeholder="First Name"
+        value={this.state.first_name}
+        onChange={this.onInputChange}
+        error={this.state.first_nameError}/>
       </div>
       <div className="field">
-        <input type="text" id="last_name" name="last_name" placeholder="Last Name"/>
+        <input type="text" id="last_name" name="last_name" placeholder="Last Name"
+        value={this.state.last_name}
+        onChange={this.onInputChange}
+        error={this.state.last_nameError}/>
       </div>
       </div>
       </div>
@@ -107,7 +318,10 @@ class NewContactForm extends Component{
 
       <div className="field">
       <label>Category</label>
-      <select className="ui fluid dropdown" id="category" name="category">
+      <select className="ui fluid dropdown" id="category" name="category"
+      value={this.state.category}
+      onChange={this.onInputChange}
+      error={this.state.categoryError}>
       <option value="">Category</option>
       <option value="Family">Family</option>
       <option value="Friends">Friends</option>
@@ -119,7 +333,10 @@ class NewContactForm extends Component{
       <label>Relationship</label>
       <div className="field">
       <div className="field">
-        <input type="text" id="relationship" name="relationship" placeholder="Relationship (e.g. Cousin, Code4Change Member, etc.)"/>
+        <input type="text" id="relationship" name="relationship" placeholder="Relationship (e.g. Cousin, Code4Change Member, etc.)"
+        value={this.state.relationship}
+        onChange={this.onInputChange}
+        error={this.state.relationshipError}/>
       </div>
       </div>
       </div>
@@ -130,7 +347,10 @@ class NewContactForm extends Component{
       <label>Birth Date</label>
         {selectedBirthDate && <p>Day: {selectedBirthDate}</p>}
         {!selectedBirthDate}
-        <DayPickerInput className="bday" onDayChange={this.handleBirthDateChange} />
+        <DayPickerInput className="bday" onDayChange={this.handleBirthDateChange}
+        value={this.state.bday}
+        onChange={this.onInputChange}
+        error={this.state.bdayError}/>
       </div>
       </div>
 
@@ -139,7 +359,10 @@ class NewContactForm extends Component{
       <div className="field">
       <div className="field">
       <label>Image</label>
-        <input type="text" name="contact_avatar" placeholder="Insert Contact Image "/>
+        <input type="text" name="contact_avatar" placeholder="Insert Contact Image "
+        value={this.state.contact_avatar}
+        onChange={this.onInputChange}
+        error={this.state.contact_avatarError}/>
       </div>
       </div>
       </div>
@@ -150,7 +373,10 @@ class NewContactForm extends Component{
       <label>Home Address</label>
       <div className="field">
       <div className="field">
-        <input type="text" name="home_address" placeholder="Street Address"/>
+        <input type="text" name="home_address" placeholder="Street Address"
+        value={this.state.home_address}
+        onChange={this.onInputChange}
+        error={this.state.home_addressError}/>
       </div>
       </div>
       </div>
@@ -158,12 +384,18 @@ class NewContactForm extends Component{
       <div className="two field">
       <div className="two field">
       <label>City</label>
-        <input type="text" name="home_city" placeholder="City"/>
+        <input type="text" name="home_city" placeholder="City"
+        value={this.state.home_city}
+        onChange={this.onInputChange}
+        error={this.state.home_cityError}/>
       </div>
       <div className="field">
 
       <label>State</label>
-      <select className="ui fluid dropdown" name="home_state">
+      <select className="ui fluid dropdown" name="home_state"
+      value={this.state.home_state}
+      onChange={this.onInputChange}
+      error={this.state.home_stateError}>
         <option value="">State</option>
       <option value="AL">Alabama</option>
       <option value="AK">Alaska</option>
@@ -220,7 +452,10 @@ class NewContactForm extends Component{
       <div className= "field">
       <div className="four wide field">
       <label>Zip Code</label>
-        <input type="text" name="home_zip" placeholder="zip code"/>
+        <input type="text" name="home_zip" placeholder="zip code"
+        value={this.state.home_zip}
+        onChange={this.onInputChange}
+        error={this.state.home_zipError}/>
       </div>
       </div>
       </div>
@@ -228,27 +463,39 @@ class NewContactForm extends Component{
       <label>Company Name</label>
       <div className="field">
       <div className="field">
-        <input type="text" name="company_name" placeholder="(e.g. Big Company Inc.)"/>
+        <input type="text" name="company_name" placeholder="(e.g. Big Company Inc.)"
+        value={this.state.company_name}
+        onChange={this.onInputChange}
+        error={this.state.company_nameError}/>
       </div>
       </div>
 
       <label>Job Title</label>
       <div className="field">
       <div className="field">
-        <input type="text" name="job_title" placeholder="(e.g. Chief Executive Officer)"/>
+        <input type="text" name="job_title" placeholder="(e.g. Chief Executive Officer)"
+        value={this.state.job_title}
+        onChange={this.onInputChange}
+        error={this.state.job_titleError}/>
       </div>
       </div>
 
 <label>Work Address</label>
 <div className="field">
 <div className="field">
-  <input type="text" name="company_address" placeholder="Street Address"/>
+  <input type="text" name="company_address" placeholder="Street Address"
+  value={this.state.company_address}
+  onChange={this.onInputChange}
+  error={this.state.company_addressError}/>
 </div>
 
 <div className="field">
 <div className="field">
 <label>Work City</label>
-  <input type="text" name="company_city" placeholder="City"/>
+  <input type="text" name="company_city" placeholder="City"
+  value={this.state.company_city}
+  onChange={this.onInputChange}
+  error={this.state.company_cityError}/>
 </div>
 </div>
 
@@ -257,7 +504,10 @@ class NewContactForm extends Component{
 <div className="field">
 <div className="field">
 <label>State</label>
-<select className="ui fluid dropdown" name="company_state">
+<select className="ui fluid dropdown" name="company_state"
+value={this.state.company_state}
+onChange={this.onInputChange}
+error={this.state.company_stateError}>
   <option value="">State</option>
 <option value="AL">Alabama</option>
 <option value="AK">Alaska</option>
@@ -313,19 +563,28 @@ class NewContactForm extends Component{
 </select>
 <div className="four wide field">
   <label>Zip Code </label>
-  <input type="text" name="company_zip" placeholder="zip code"/>
+  <input type="text" name="company_zip" placeholder="zip code"
+  value={this.state.company_zip}
+  onChange={this.onInputChange}
+  error={this.state.company_zipError}/>
 </div>
 </div>
 
 
 <div className="field">
   <label>Cell # </label>
-  <input type="text" name="cell_num" placeholder="(e.g. 555-555-5555)"/>
+  <input type="tel" name="cell_num" placeholder="(e.g. 555-555-5555)"
+  value={this.state.cell_num}
+  onChange={this.onInputChange}
+  error={this.state.cell_numError}/>
 </div>
 
 <div className="field">
   <label>Email </label>
-  <input type="text" name="email_address" placeholder="(e.g. first.last@email.com)"/>
+  <input type="email" name="email_address" placeholder="(e.g. first.last@email.com)"
+  value={this.state.email_address}
+  onChange={this.onInputChange}
+  error={this.state.email_addressError}/>
 </div>
 
 
@@ -340,17 +599,47 @@ class NewContactForm extends Component{
       <label>Date of Last Event:</label>
         {selectedEventDate && <p>Day: {selectedEventDate}</p>}
         {!selectedEventDate}
-        <DayPickerInput className="last_event_date" onDayChange={this.handleDayChange} />
+        {/*try setting max date to be current date since last event should not be able to exist in the future, hard codingfor now*/}
+        <DayPickerInput type="date" max = "2019-01-31" className="last_event_date" onDayChange={this.handleDayChange}
+        value={this.state.last_event_date}
+        onChange={this.onInputChange}
+        error={this.state.last_event_dateError}/>
       </div>
       </div>
 
       <div className="field">
         <label>Notes: </label>
-        <textarea type="text field" name="notes"></textarea>
+        <textarea type="text field" name="notes"
+        value={this.state.notes}
+        onChange={this.onInputChange}
+        error={this.state.notesError}></textarea>
       </div>
 
 
-      <input className="ui button fluid" tabIndex="0" type="submit" value="Submit"/>
+      <input className="ui button fluid" tabIndex="0" type="submit" value="Submit"
+      disabled={!this.state.first_name||
+                !this.state.last_name||
+                !this.state.selectedBirthDate||
+                !this.state.contact_avatar||
+                !this.state.category||
+                !this.state.selectedEventDate||
+                !this.state.relationship||
+                !this.state.home_address||
+                !this.state.home_city||
+                !this.state.home_state||
+                !this.state.home_zip||
+                !this.state.company_name||
+                !this.state.job_title||
+                !this.state.company_address||
+                !this.state.company_city||
+                !this.state.company_state||
+                !this.state.company_zip||
+                !this.state.cell_num||
+                !this.state.email_address||
+                !this.state.notes
+
+
+      }/>
 
       </form>
       </div>
